d619ebf1eac8400fce75b1b27b66916b8ccddac1
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2026-02-21 15:21:36 -0800

    Finally made isWii toggle state accessible after scene changes

--- Scripts/ControllerManager.cs
+++ Scripts/ControllerManager.cs
@@ -7,10 +7,12 @@ public class ControllerManager : MonoBehaviour
 {
     public Toggle toggle;
     public Color gray = new Color(0.35686274509f, 0.31764705882f, 0.29411764705f);
+    public ControllerData controllerData;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Assign listener to detect when toggle is selected/unselected
         toggle = GetComponent<Toggle>();
         toggle.onValueChanged.AddListener(OnToggleValueChanged);
         OnToggleValueChanged(toggle.isOn);
@@ -18,10 +20,18 @@ public class ControllerManager : MonoBehaviour
 
     void OnToggleValueChanged(bool isOn)
     {
-        // targetGraphic is the background image of the toggle
-        if (toggle.targetGraphic != null)
+        // Change background color of checkmark when toggle is selected/unselected
+        if (toggle.targetGraphic != null) // targetGraphic is the background image of the toggle
         {
             toggle.targetGraphic.color = isOn ? gray : Color.white;
+        }
+
+        // Update isWii variable (part of empty game object that won't destroy when scene changes)
+        controllerData = GameObject.Find("ChosenController").GetComponent<ControllerData>();
+        if (controllerData != null && gameObject.name == "WiiToggle")
+        {
+            controllerData.isWii = toggle.isOn;
+            Debug.Log("isWii: " + controllerData.isWii);
         }
     }
 }
